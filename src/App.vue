e6927058b2d284d4d97c2d2d164186c038b2fb72
ellascreations <maltys@live.com.au>
2025-03-05 19:55:53 +1100

    new site

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="App">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data() {
+    return {
+      shoppingData: [],
+    };
+  },
+
+  created: function () {
+    this.getShoppingData();
+  },
+
+  methods: {
+    getShoppingData() {
+      fetch("https://fakestoreapi.com/products")
+        .then((response) => response.json())
+        .then((JSON) => (this.shoppingData = JSON));
+    },
+
+    logStuff() {
+      console.log(this.shoppingData);
+    },
+  },
+};
+</script>
+
+<style>
+/* header font*/
+@import url("https://fonts.googleapis.com/css2?family=Rouge+Script&display=swap");
+/* text font*/
+@import url("https://fonts.googleapis.com/css2?family=Trirong:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
+body {
+  background-color: #dddad0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: stretch;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navbar">
+    <RouterLink to="/">
+      <h1>Ella's Creations</h1>
+    </RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 55px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #dddad0;
+  z-index: 9;
+  text-decoration: none;
+}
+
+.navbar h1 {
+  font-family: "Rouge Script", cursive;
+  font-weight: bold;
+  font-size: 50px;
+}
+</style>
