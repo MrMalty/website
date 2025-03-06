3c70c184f60ac715301abafbe37de8f7780c4a85
ellascreations <maltys@live.com.au>
2025-03-06 11:33:30 +1100

    added more to site

--- components/ProductComponent.vue
+++ components/ProductComponent.vue
@@ -5,6 +5,39 @@
   >
     <NuxtLink :to="`/item/${product.id}`">
       <img class="rounded-t" :src="product.url" />
+      <div id="ProductDetails">
+        <span class="flex items-center justify-start gap-3 px-1 pt-1">
+          <span class="text-[#FF6674] font-semibold">{{ priceComputed }}</span>
+          <span class="text-gray500 text-sm text-light line-through">{{
+            oldPriceComputed
+          }}</span>
+        </span>
+        <span
+          class="px-1 relative -top-1.5 text-[#FF6674] text-xs font-semibold"
+        >
+          Extra 5% off</span
+        >
+        <div class="flex items-center gap-1 px-1 relative -top-1">
+          <span
+            class="bg-[#FD374F] text-white text-[9px] font-semibold px-1.5 rounded-sm"
+          >
+            Welcome Deal</span
+          >
+          <span
+            class="bg-[#f5f5f5] border text-[#c08562] text-[9px] font-semibold px-1.5 rounded-sm"
+            >Top Selling</span
+          >
+        </div>
+        <P class="flex items-center px-1 pt-0.5 text-xs text-[#252525]">
+          5,000+ sold
+          <icon
+            name="material-symbols:star-rate"
+            color="#F8D16B"
+            class="ml-1.5"
+          />
+          4.7</P
+        >
+      </div>
     </NuxtLink>
   </div>
 </template>
@@ -17,7 +50,7 @@ const priceComputed = computed(() => {
   return product.value.price / 100;
 });
 
-const olPriceComputed = computed(() => {
+const oldPriceComputed = computed(() => {
   let res = (product.value.price + product.value.price / 20) / 100;
   return res.toFixed(2);
 });
