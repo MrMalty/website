f229441c33d23b7576e9ad92a0ece823a2b7adf7
ellascreations <maltys@live.com.au>
2025-03-10 21:09:18 +1100

    updating

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -50,8 +50,56 @@
           </div>
           <div ID="Items" class="bg-white rounded-lg p-4 mt-4">
             <div v-for="product in products">
-              <CheckoutItem :Product="product" />
+              <CheckoutItem :product="product" />
             </div>
+          </div>
+        </div>
+
+        <div class="md:hidden block my-4" />
+
+        <div class="md:w-[35%]">
+          <div id="PlaceOrder" class="bg-white round-lg p-4">
+            <div class="text-2xl font-exrtabold mb-2">Summary</div>
+            <div class="flex items center justify-between my-4">
+              <div>Total Shipping</div>
+              <div>Free</div>
+            </div>
+
+            <div class="border-t" />
+
+            <div class="flex items-center justify-between my-4">
+              <div class="font-semibold">Total</div>
+              <div class="text-2xl font semibold">
+                $<span class="font-extrabold">{{ total / 100 }}</span>
+              </div>
+            </div>
+
+            <form @submit.prevent="pay()">
+              <div
+                class="border border-grey-500 p-2 rounded-sm"
+                id="card-element"
+              />
+              <p
+                id="card-error"
+                role="alert"
+                class="text-red-700 text-center font-semibold"
+              />
+              <button
+                :disabled="isProcessing"
+                type="submit"
+                class="mt-4 bg-gradient-to-r from-[#F9D36B] to-[#F7E5B9] w-full text-[#535960] text-[21px] font-semibold p-1.5 rounded-full"
+                :class="isProcessing ? 'opacity-70' : 'opacity-100'"
+              >
+                <Icon v-if="isProcessing" name="eos-icons:loading" />
+                <div v-else>Place Order</div>
+              </button>
+            </form>
+          </div>
+          <div class="bg-white rounded-lg p-4 mt-4">
+            <div class="text-lg font-semibold mb-2 mt-2">Ella's Creations</div>
+            <p class="my-2">
+              Ella's Creations keeps your information and payment safe
+            </p>
           </div>
         </div>
       </div>
