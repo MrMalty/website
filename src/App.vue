243ebddc4e76bbca1c7ef018adc1312e976dcb89
ellascreations <maltys@live.com.au>
2025-03-13 17:54:02 +1100

    Restarting

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="max-w-full">
+    <div class="flex items-center justify-between h-[120px]">
+      <div>
+        <img class="w-[300px]" src="./assets/Ellas-Creations.png" />
+      </div>
+      <div>Menu Here</div>
+    </div>
+    <div>Body</div>
+  </div>
+</template>
+
+<style scoped></style>
