72a6701cd06cdadf48bae3352fb66bb9ff6b85c2
ellascreations <maltys@live.com.au>
2025-03-13 18:03:52 +1100

    yup

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "main-layout"
-declare module "F:/Sites/ellascreations/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
