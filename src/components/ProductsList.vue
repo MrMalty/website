ccca656494108e2e55b6e5c00022b155c66c8f3b
ellascreations <maltys@live.com.au>
2025-03-18 21:37:00 +1100

    break points on images

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid-wrap">
     <div class="product-item" v-for="product in products" :key="product.id">
-      <img :src="product.imageName" />
+      <img class="w-16 md:w-32 lg:48" :src="product.imageName" />
       <h3 class="product-name">{{ product.name }}</h3>
       <p class="product-price">{{ product.price }}</p>
       <router-link :to="'/products/' + product.id">
