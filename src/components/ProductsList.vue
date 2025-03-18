81f7fee321613e1ec3a9c1e63a947aa8a67730e3
ellascreations <maltys@live.com.au>
2025-03-18 21:45:46 +1100

    scaling images

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="grid-wrap">
     <div class="product-item" v-for="product in products" :key="product.id">
-      <img class="w-16 md:w-32 lg:48" :src="product.imageName" />
+      <img :src="product.imageName" />
       <h3 class="product-name">{{ product.name }}</h3>
       <p class="product-price">{{ product.price }}</p>
       <router-link :to="'/products/' + product.id">
-        <button class="md:text-sm lg:text-xl">View Details</button>
+        <button>View Details</button>
       </router-link>
     </div>
   </div>
