8eca7fb6524c5b7eab5f265be045b94f5755ad9c
ellascreations <maltys@live.com.au>
2025-05-12 16:54:35 +1000

    .

--- server/api/prisma/get-all-products.js
+++ server/api/prisma/get-all-products.js
@@ -1,15 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
 export default defineEventHandler(async (event) => {
-try {
 
-        const prisma = new PrismaClient();
+    const prisma = new PrismaClient();
         
     let products = await prisma.products.findMany()
         console.log(products)
         return products
-   
-} catch (error) {
-    console.error(error)
-}
+
 }) 
