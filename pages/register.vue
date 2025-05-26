eef195d38d7f4eda547c38ab479f04209dd7761d
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-05-26 22:17:22 +1000

    register page update

--- pages/register.vue
+++ pages/register.vue
@@ -1,27 +1,38 @@
 <!-- pages/signup.vue -->
 <template>
-    <div>
-      <h2>Create Account</h2>
+      <div id="AuthPage" class="w-full h-[100vh] bg-white">
+        <div class="w-full flex items-center justify-center p-5 border-b border-b-gray-300">
+            <NuxtLink to="/" class="min-w-[170px]">
+                <img width="170" src="/kc_logo.png">
+            </NuxtLink>
+        </div>
+
+        <div class="max-w-[400px] mx-auto px-2">
+
+          <div>
+            <h2>Create Account</h2>
       
-      <form @submit.prevent="handleSignUp">
-        <div>
-          <label>Email</label>
-          <input type="email" v-model="email" required/>
+            <form @submit.prevent="handleSignUp">
+            <div>
+              <label>Email</label>
+              <input type="email" v-model="email" required/>
+            </div>
+  
+            <div>
+             <label>Password</label>
+              <input type="password" v-model="password" required/>
+            </div>
+  
+            <button type="submit" :disabled="loading"> {{ loading ? 'Creating Account...' : 'Sign Up' }}</button>
+  
+            <p v-if="error">{{ error }}</p>
+            </form>
+  
+            <p> Already have an account? <NuxtLink to="/login">Login</NuxtLink>
+            </p>
+          </div>
         </div>
-  
-        <div>
-          <label>Password</label>
-          <input type="password" v-model="password" required/>
-        </div>
-  
-        <button type="submit" :disabled="loading"> {{ loading ? 'Creating Account...' : 'Sign Up' }}</button>
-  
-        <p v-if="error">{{ error }}</p>
-      </form>
-  
-      <p> Already have an account? <NuxtLink to="/login">Login</NuxtLink>
-      </p>
-    </div>
+      </div>div>
   </template>
   
   <script setup>
