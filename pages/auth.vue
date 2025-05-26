d152e8afe12e9b2d09ebf3da15ad82507bdf6eae
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-05-27 09:12:04 +1000

    .

--- pages/auth.vue
+++ pages/auth.vue
@@ -21,6 +21,7 @@
                 </form>
             </div>
             <div class="mt-3 mb-3 border-b-2 "/>
+            <p>register with email if you do not have an account, <NutLink to="/register">Click Here.</NuxtLink></p>
 
             <button 
                 @click="login('google')"
