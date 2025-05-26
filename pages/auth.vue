d8a2f60f1a1cf88de1a61cbdda10a929891bcbf1
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-05-27 09:21:39 +1000

    .

--- pages/auth.vue
+++ pages/auth.vue
@@ -8,21 +8,8 @@
 
         <div class="max-w-[400px] mx-auto px-2">
             <div class="text-center my-6">Login / Register</div>
-            <div>
-                <form @click="handleLogin">
-                <div class="flex items-center border-2 border-[#23AAEF] rounded-md w-full">
-                    <input class="w-full" placeholder="Email" type="email"  v-model="email" required/>
-                </div>
-                <div class="flex items-center border-2 border-[#23AAEF] rounded-md w-full">
-                    <input class="w-full mt-2" placeholder="Passowrd" type="password" v-model="password" required/>
-                </div>
-                <button class="flex items-center justify-center bg-[#0E2167] w-full text-[#FFFFFF] text-[21px] font-semibold p-1.5 rounded-full mt-2" type="submit" :disabled="loading"> {{ loading ? 'Logging in...' : 'Login' }}</button>
-                <p v-if="error">{{ error }}</p>
-                </form>
-            </div>
-            <div class="mt-3 mb-3 border-b-2 "/>
-            <p>To register, <NuxtLink to="/register">Click Here.</NuxtLink></p>
 
+<!-- google login section -->
             <button 
                 @click="login('google')"
                 class="
@@ -42,6 +29,23 @@
                 <img class="w-full max-w-[30px]" src="/google-logo.png">
                 <div>Google</div>
             </button>
+
+<!-- Email Loging Section -->
+            <div>
+                <form @click="handleLogin">
+                <div class="flex items-center border-2 border-[#23AAEF] rounded-md w-full">
+                    <input class="w-full" placeholder="Email" type="email"  v-model="email" required/>
+                </div>
+                <div class="flex items-center border-2 border-[#23AAEF] rounded-md w-full">
+                    <input class="w-full mt-2" placeholder="Passowrd" type="password" v-model="password" required/>
+                </div>
+                <button class="flex items-center justify-center bg-[#0E2167] w-full text-[#FFFFFF] text-[21px] font-semibold p-1.5 rounded-full mt-2" type="submit" :disabled="loading"> {{ loading ? 'Logging in...' : 'Login' }}</button>
+                <p v-if="error">{{ error }}</p>
+                </form>
+            </div>
+            <div class="mt-3 mb-3 border-b-2 "/>
+            <p>To register, <NuxtLink to="/register">Click Here.</NuxtLink></p>
+
         </div>
     </div>
 </template>
@@ -51,22 +55,22 @@ const user = useSupabaseUser();
 
 const handleLogin = async () => {
     try {
-      loading.value = true;
-      error.value = null;
+      loading.value = true
+      error.value = null
       
       const { data, error: loginError } = await supabase.auth.signInWithPassword({
         email: email.value,
         password: password.value
       });
   
-      if (loginError) throw loginError;
+      if (loginError) throw loginError
   
       // Redirect to Confirm Page to check user info
       navigateTo('/');
     } catch (err) {
-      error.value = err.message;
+      error.value = err.message
     } finally {
-      loading.value = false;
+      loading.value = false
     }
   };
 
