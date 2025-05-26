026bc0f95771c481383c2867021633518b3c396c
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-05-27 08:51:49 +1000

    .

--- pages/auth.vue
+++ pages/auth.vue
@@ -8,6 +8,7 @@
 
         <div class="max-w-[400px] mx-auto px-2">
             <div class="text-center my-6">Login / Register</div>
+
                 <div>
                     <form @submit.prevent="handleLogin">
                         <div>
@@ -19,51 +20,31 @@
                             <label>Password</label>
                             <input type="password" v-model="password" required/>
                         </div>
-                </div>
                     <button type="submit" :disabled="loading"> {{ loading ? 'Logging in...' : 'Login' }}</button>
                     <p v-if="error">{{ error }}</p>
                     </form>
-            <button 
-                @click="login('google')"
-                class="
-                    flex 
-                    items-center 
-                    justify-center 
-                    gap-3
-                    p-1.5
-                    w-full 
-                    border 
-                    hover:bg-gray-100
-                    rounded-full
-                    text-lg
-                    font-semibold
-                "
-            >
-                <img class="w-full max-w-[30px]" src="/google-logo.png">
-                <div>Google</div>
-            </button>
-<!--
-            <button 
-                @click="login('github')"
-                class="
-                mt-4
-                    flex 
-                    items-center 
-                    justify-center 
-                    gap-3
-                    p-1.5
-                    w-full 
-                    border 
-                    hover:bg-gray-100
-                    rounded-full
-                    text-lg
-                    font-semibold
-                "
-            >
-                <img class="w-full max-w-[30px]" src="/github-logo.png">
-                <div>Github</div>
-            </button> -->
+                </div>
 
+                <button 
+                    @click="login('google')"
+                    class="
+                        flex 
+                        items-center 
+                        justify-center 
+                        gap-3
+                        p-1.5
+                        w-full 
+                        border 
+                        hover:bg-gray-100
+                        rounded-full
+                        text-lg
+                        font-semibold
+                    "
+                >
+                    <img class="w-full max-w-[30px]" src="/google-logo.png">
+                    <div>Google</div>
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -91,7 +72,7 @@ const handleLogin = async () => {
       loading.value = false
     }
   }
-  
+
 watchEffect(() => {
     if (user.value) {
         return navigateTo('/')
