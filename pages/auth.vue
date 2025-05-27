cfbfecf0ef2dfbc296d563332574aef77b30c7dc
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-05-27 14:46:16 +1000

    .

--- pages/auth.vue
+++ pages/auth.vue
@@ -42,7 +42,7 @@
                     <input class="w-full text-gray-800 border text-sm border-[#EFF0EB] rounded-lg p-3 placeholder-gray-500 focus:outline-none bg-[#FFFFFF]" placeholder="Passowrd" type="password" v-model="password" required/>
                 </div>
                 <button
-                @click="login"
+                @click="loginemail"
                 :disabled="loading"
                 type="submit"
                 class="flex items-center justify-center bg-[#FFFFFF] w-full border-2 hover:bg-gray-100 border-[#0E2167] text-[#0E2167] text-[21px] font-semibold p-1.5 rounded-full mt-2"
@@ -65,7 +65,7 @@
 const client = useSupabaseClient();
 const user = useSupabaseUser();
 
-async function login() {
+async function loginemail() {
   const { email, password } = credentials;
   const { error } = await client.auth.signInWithPassword({ email, password });
   if (!error) return router.push('/');
