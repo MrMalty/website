3821267391c69931e4b330ab2c971d20b9eeb1cc
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-05-30 08:18:36 +1000

    .

--- server/api/prisma/get-all-orders-by-user/[id].js
+++ server/api/prisma/get-all-orders-by-user/[id].js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient()
 export default defineEventHandler(async (event) => {
 
     let orders = await prisma.orders.findMany({
-        where: { userId: context.params.id },
+        where: { userId: event.context.params.id },
         orderBy: { id: "desc" },
         include: { 
             orderItem: {
