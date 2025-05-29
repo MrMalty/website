1db170d6123209d3ea15aafa3e9acd58b247d80b
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-05-30 08:43:16 +1000

    .

--- server/api/prisma/get-all-orders-by-user/[id].js
+++ server/api/prisma/get-all-orders-by-user/[id].js
@@ -2,15 +2,7 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
-  try {
     const userId = event.context.params.userId
-
-    if (!userId) {
-      return {
-        statusCode: 400,
-        body: { error: 'Missing userId parameter' }
-      }
-    }
 
     const orders = await prisma.orders.findMany({
       where: { userId },
@@ -25,11 +17,4 @@ export default defineEventHandler(async (event) => {
     })
 
     return orders
-  } catch (error) {
-    console.error('Error fetching orders:', error)
-    return {
-      statusCode: 500,
-      body: { error: 'Internal Server Error' }
-    }
-  }
 })
