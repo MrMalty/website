daa3c252c174cc64c7c7164d8e7fbc93774f3894
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-05-30 14:09:30 +1000

    .

--- server/api/prisma/get-all-orders-by-user/[id].js
+++ server/api/prisma/get-all-orders-by-user/[id].js
@@ -6,26 +6,26 @@ export default defineEventHandler(async(event) => {
     const userId = event.context.params.id
     return { UersId: 'users ID is ${userId}' }
 
-    if (!userId) {
-      return {
-        statusCode: 400,
-        body: { error: 'Missing userId parameter' }
-      }
-    }
+    // if (!userId) {
+    //   return {
+    //     statusCode: 400,
+    //     body: { error: 'Missing userId parameter' }
+    //   }
+    // }
 
-    const orders = await prisma.orders.findMany({
-      where: { userId },
-      orderBy: { id: 'desc' },
-      include: {
-        orderItem: {
-          include: {
-            product: true
-          }
-        }
-      }
-    })
+    // const orders = await prisma.orders.findMany({
+    //   where: { userId },
+    //   orderBy: { id: 'desc' },
+    //   include: {
+    //     orderItem: {
+    //       include: {
+    //         product: true
+    //       }
+    //     }
+    //   }
+    // })
 
-    return {orders, userId }
+    // return {orders, userId }
   } catch (error) {
     console.error('Error fetching orders:', error)
     return {
