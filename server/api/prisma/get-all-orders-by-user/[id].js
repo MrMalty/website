747047e500f4ed9caa24daeebabc2a972fb9e5a9
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-05-30 15:37:57 +1000

    .

--- server/api/prisma/get-all-orders-by-user/[id].js
+++ server/api/prisma/get-all-orders-by-user/[id].js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient()
 export default defineEventHandler(async (event) => {
 
 const userid = event.context.params.id
-return { Message: `users ID is ${string(userid.value)}` }
+return { Message: `users ID is ${userid.value}` }
   // try {
   //   const userid = event.context.params
 
