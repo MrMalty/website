619658aa320455f47da7ba901d8c7858993d58c5
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-05-31 21:56:29 +1000

    .

--- pages/orders.vue
+++ pages/orders.vue
@@ -7,7 +7,7 @@
                     <span class="pl-4">Orders</span>
                 </div>
                 <div>{{ user.id }}</div>
-                <div v-if="user.value">{{ user.value}}</div>
+                <div v-if="user.value.id">{{ user.value.id }}</div>
                 <div 
                     v-if="orders && orders.data" 
                     v-for="order in orders.data" 
