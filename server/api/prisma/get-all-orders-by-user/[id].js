01325e21f7c536fc7640d8dc616240c80673b5f6
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-06-01 21:06:47 +1000

    .

--- server/api/prisma/get-all-orders-by-user/[id].js
+++ server/api/prisma/get-all-orders-by-user/[id].js
@@ -1,26 +1,41 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-const userId = event.context.params.userId;
-if (!userId) {
-    console.error("User ID is missing or invalid.");
-    // return { error: "User ID is required." };
-}
-
 export default defineEventHandler(async (event) => {
-    try {
+    const userId = event.context.params.userId;
+    console.log("User ID from params:", userId);
+
+    if (!userId) {
+        return { error: "Missing userId parameter." };
+    };
+
         let orders = await prisma.orders.findMany({
             where: { userId: userId },
             orderBy: { id: "desc" },
             include: { 
                 orderItem: {
                     include: { product: true }
-                }
+                },
             }
-        })
+        });
         return orders
-    } catch (error) {
-        console.error("Error fetching orders:", error)
-        return { error: "Failed to retrieve orders" }
-    }
-})
+});
+
+
+// export default defineEventHandler(async (event) => {
+//     try {
+//         let orders = await prisma.orders.findMany({
+//             where: { userId: event.context.params.userId },
+//             orderBy: { id: "desc" },
+//             include: { 
+//                 orderItem: {
+//                     include: { product: true }
+//                 }
+//             }
+//         })
+//         return orders
+//     } catch (error) {
+//         console.error("Error fetching orders:", error)
+//         return { error: "Failed to retrieve orders" }
+//     }
+// })
