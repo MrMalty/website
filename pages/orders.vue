dcfb44ad60c7e6f575a7344c01fd9360a5c6af8b
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-06-01 18:25:49 +1000

    .

--- pages/orders.vue
+++ pages/orders.vue
@@ -55,7 +55,7 @@ onMounted(() => {
     orders.value = useFetch(`/api/prisma/get-all-orders-by-user/${user.id}`);
     // setTimeout(() => (userStore.isLoading = false), 200);
   }
-  console.log("Orders:", orders.value);
+  console.log("Orders:", orders.value.data);
 });
 
 
