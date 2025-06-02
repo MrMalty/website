f75ef3e3b80191b7239686cdf59fc9961e70de54
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-06-02 11:26:38 +1000

    .

--- server/api/prisma/get-all-orders-by-user/[id].js
+++ server/api/prisma/get-all-orders-by-user/[id].js
@@ -1,32 +1,18 @@
-
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
-  const userId = event.context.params?.userId;
-  console.log("User ID:", userId);
-
-  if (!userId) {
-    return { error: "Missing userId parameter." };
-  }
-
-  try {
-    const orders = await prisma.orders.findMany({
-      where: { userId },
-      orderBy: { id: "desc" },
-      include: {
-        orderItem: {
-          include: {
-            product: true
-          }
-        }
-      }
-    });
-
-    console.log("Orders:", orders);
-    return orders;
-  } catch (error) {
-    console.error("Prisma error:", error);
-    return { error: "Failed to fetch orders." };
-  }
+    let orders = await prisma.orders.findMany({
+        where: { userId: event.context.params.userId },
+        orderBy: { id: "desc" },
+        include: { 
+            orderItem: {
+                include: {
+                    product: true
+                }
+            }
+            
+        }
+    });
+    return orders;
 });
