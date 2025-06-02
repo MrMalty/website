cf94f00d8e5f18ca1a69cd09bda1943e26c8f80a
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-06-02 20:58:15 +1000

    .

--- server/api/prisma/get-all-orders-by-user/[id].js
+++ server/api/prisma/get-all-orders-by-user/[id].js
@@ -5,17 +5,19 @@ return { message: `Fetching data for ID: ${id}` };
 
 
 export default defineEventHandler(async (event) => {
-    let orders = await prisma.orders.findMany({
-        where: { userId: id },
-        orderBy: { id: "desc" },
-        include: { 
-            orderItem: {
-                include: {
-                    product: true
-                }
-            }
+const { id } = event.context.params
+return { message: `Fetching data for ID: ${id}`};
+    // let orders = await prisma.orders.findMany({
+    //     where: { userId: id },
+    //     orderBy: { id: "desc" },
+    //     include: { 
+    //         orderItem: {
+    //             include: {
+    //                 product: true
+    //             }
+    //         }
             
-        }
-    });
-    return orders;
+    //     }
+    // });
+    // return orders;
 });
