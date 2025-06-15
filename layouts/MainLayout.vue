0ac45ef313270385ffda643bbe13fc96b1f3ec92
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-06-16 08:12:46 +1000

    .

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -61,7 +61,7 @@
             </div>
             <div class="border-b" />
             <ul class="bg-white">
-              <li v-if="user">Welcome {{ user.display_name }}</li>
+              <li v-if="user">Welcome {{ user }}</li>
               <li
                 @click="navigateTo('/orders')"
                 class="text-[13px] py-2 px-4 w-full hover:bg-gray-200"
