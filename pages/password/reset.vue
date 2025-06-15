326b94656bf0137376eacea8c6fb7484347016fa
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-06-15 14:46:08 +1000

    .

--- pages/password/reset.vue
+++ pages/password/reset.vue
@@ -11,13 +11,21 @@ const requestResetPassword = async () => {
 </script>
 
 <template>
-  <div>
-    <input
-      v-model="email"
-      type="email"
-    />
-    <button @click="requestResetPassword">
-      Reset Password
-    </button>
-  </div>
+    <div id="ResetPage" class="w-full h-[100vh] bg-white">
+        <div class="w-full flex items-center justify-center p-5 border-b border-b-gray-300">
+            <NuxtLink to="/" class="min-w-[170px]">
+                <img width="170" src="/kc_logo.png">
+            </NuxtLink>
+        </div>
+
+        <div class="max-w-[400px] mx-auto px-2">
+            <input
+                v-model="email"
+                type="email"
+            />
+            <button @click="requestResetPassword">
+                Reset Password
+            </button>
+        </div>
+    </div>
 </template>
