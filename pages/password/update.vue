407c81dce470dd6e9f15f91415293a51a2bc4995
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-06-15 14:56:26 +1000

    .

--- pages/password/update.vue
+++ pages/password/update.vue
@@ -11,13 +11,21 @@ const updateUserPassword = async () => {
 </script>
 
 <template>
-  <div>
-    <input
-      v-model="newPassword"
-      type="password"
-    />
-    <button @click="updateUserPassword">
-      Update Password
-    </button>
-  </div>
+    <div id="ResetPage" class="w-full h-[100vh] bg-white">
+        <div class="w-full flex items-center justify-center p-5 border-b border-b-gray-300">
+            <NuxtLink to="/" class="min-w-[170px]">
+                <img width="170" src="/kc_logo.png">
+            </NuxtLink>
+        </div>
+
+        <div class="max-w-[400px] mx-auto px-2 mt-10">
+            <input
+                v-model="newPassword"
+                type="password"
+            />
+            <button @click="updateUserPassword">
+                Update Password
+            </button>
+        </div>
+    </div>
 </template>
