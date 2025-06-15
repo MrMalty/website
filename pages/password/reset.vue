a7245b117662ad6696536b38541034a42b0af4a4
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2025-06-16 07:58:03 +1000

    .

--- pages/password/reset.vue
+++ pages/password/reset.vue
@@ -23,7 +23,7 @@ const requestResetPassword = async () => {
 
         <div class="max-w-[400px] mx-auto px-2 mt-10">
             <input class="w-full text-gray-800 border text-sm border-[#EFF0EB] rounded-lg p-3 placeholder-gray-500 focus:outline-none bg-[#FFFFFF]"
-                placeholder="Enter new password"
+                placeholder="Enter your account email address?"
                 v-model="email"
                 type="email"
             />
