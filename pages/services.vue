8484c23cb53aeff0c75ce26e1ede35285efa0af2
Ella's Creations <91229538+MrMalty@users.noreply.github.com>
2025-09-02 21:07:32 +1000

    services

--- pages/services.vue
+++ pages/services.vue
@@ -65,7 +65,7 @@ const pricing = [
     </LandingSectionhead>
 
     <div
-      class="grid md:grid-flow-col auto-rows-[550px] gap-10 mx-auto max-w-screen-lg mt-12"
+      class="grid md:grid-flow-col gap-10 mx-auto max-w-screen-lg mt-12"
     >
       <LandingPricing v-for="item of pricing" :plan="item" />
     </div>
