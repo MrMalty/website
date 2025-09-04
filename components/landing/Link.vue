9b52b423af42b4d2c7e9a3961b6b3188905b8f43
Ella's Creations <91229538+MrMalty@users.noreply.github.com>
2025-09-04 10:17:48 +1000

    .

--- components/landing/Link.vue
+++ components/landing/Link.vue
@@ -30,7 +30,7 @@ const styles = {
 
 <template>
   <NuxtLink
-    :href="href"
+    :to="href"
     :class="[
       'rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200',
       block && 'w-full',
