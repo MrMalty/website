df87431682fc26225dc7c8ca0bee3432e8238fb5
Ella's Creations <91229538+MrMalty@users.noreply.github.com>
2025-09-04 10:36:02 +1000

    .

---
+++ components/landing/Drc.vue
@@ -0,0 +1,48 @@
+<template>
+  <main class="grid lg:grid-cols-2 place-items-center pt-16 pb-8 md:pt-8">
+    <div class="p-24 md:order-1 hidden md:block">
+      <img
+        class="rounded-full transition-transform transform hover:scale-125"
+        src="~/assets/img/kc1.jpg"
+        loading="eager"
+        format="avif"
+        width="512"
+        height="512"
+      />
+    </div>
+
+    <div>
+      <h1
+        class="text-4xl lg:text-6xl xl:text-7xl font-bold lg:tracking-tight text-MainHeader"
+      >
+        Data Recovery.
+      </h1>
+      <p class="text-lg mt-4 text-Primary max-w-xl">
+        We specialize in recovering lost, deleted, corrupted, or inaccessible data
+        from a wide range of storage devices. Whether the cause is accidental deletion, 
+        hardware failure, system crash, or malware infection, our advanced tools and 
+        techniques give you the best chance of restoring valuable information.
+      </p>
+    </div>
+    <div>
+      <ul>
+        <li>Hard Drive & SSD Recovery</li>
+        <li>RAID & Server Recovery</li>
+        <li>USB & External Drive Recovery</li>
+        <li>Laptop & Desktop Data Retrieval</li>
+        <li>Recovery from Malware or Ransomware Attacks</li>
+      </ul>  
+
+    </div>
+    <!-- <LandingSectionhead>
+      <template v-slot:title>Data Recovery</template>
+      <template v-slot:desc>We specialize in recovering lost, deleted, corrupted, or inaccessible data
+                            from a wide range of storage devices.<br />Whether the cause is accidental deletion, 
+                            hardware failure, system crash, or malware infection, our advanced tools and 
+                            techniques<br />give you the best chance of restoring valuable information.</template>
+    </LandingSectionhead> -->
+</main>
+</template>
+
+
+
--- pages/datarecovery.vue
+++ pages/datarecovery.vue
@@ -6,25 +6,6 @@ definePageMeta({
 
 <template>
   <LandingContainer>
-    <LandingSectionhead>
-      <template v-slot:title>Data Recovery</template>
-      <template v-slot:desc>We specialize in recovering lost, deleted, corrupted, or inaccessible data
-                            from a wide range of storage devices.<br />Whether the cause is accidental deletion, 
-                            hardware failure, system crash, or malware infection, our advanced tools and 
-                            techniques<br />give you the best chance of restoring valuable information.</template>
-    </LandingSectionhead>
-
-    <div class="flex flex-col gap-3 mx-auto max-w-4xl mt-16">
-      <h2 class="font-bold text-3xl text-Primary">
-        Our Data Recorvery servces include:
-      </h2>
-      <ul>
-        <li>Hard Drive & SSD Recovery</li>
-        <li>RAID & Server Recovery</li>
-        <li>USB & External Drive Recovery</li>
-        <li>Laptop & Desktop Data Retrieval</li>
-        <li>Recovery from Malware or Ransomware Attacks</li>
-      </ul>  
-    </div>
+    <LandingDrc></LandingDrc>
   </LandingContainer>
 </template>
