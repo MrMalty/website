fb8d7cb1282b5acd4ecf209b06c4dbb92dcd7fb6
ellascreations <maltys@live.com.au>
2025-09-08 22:02:18 +1000

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -9,8 +11,42 @@ export default {
     "./error.vue",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    extend: {
+      colors: {
+        Primary: "#404E71",
+        PrimaryLight: "#2CB6D5",
+        MainHeader: "#4DBDE8",
 
+        PageHeader: "#404E71",
+        PageHeaderLight: "#2CB6D5",
+
+        InputPrimary: "#2CB6D5",
+        InputBorder: "#2CB6D5",
+        InputBorderFocus: "#404E71",
+        InputBorderRing: "#2CB6D5",
+        ButtonPrimary: "#596F95",
+        ButtonPrimaryHover: "#8BD0DF",
+        ButtonPrimaryHoverText: "#596F95",
+        ButtonOutline: "#596F95",
+        ButtonOutlineHover: "#8BD0DF",
+        ButtonOutlineHoverText: "#596F95",
+
+        ButtonLight: "#8BD0DF",
+        ButtonPrimaryText: "#8BD0DF",
+        ButtonText: "#596F95",
+
+        IconColor: "#BED2EF",
+
+        ServiceHeader: "#404E71",
+        ServicePrice: "#BED2EF",
+        ServiceItems: "#404E71",
+
+        FooterText: "#2CB6D5",
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
