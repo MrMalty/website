be055b46c0ace18733b2bf41d8b38037ed885b36
Ella's Creations <91229538+MrMalty@users.noreply.github.com>
2025-09-08 18:57:44 +1000

    New Page

---
+++ pages/virusclean.vue
@@ -0,0 +1,16 @@
+<script setup>import { useHead } from '#app';
+
+useHead({
+  title: 'Kialla Computers - Virus Clean',
+});
+
+definePageMeta({
+  layout: "landing",
+});
+</script>
+
+<template>
+  <LandingContainer>
+    <LandingVcc></LandingVcc>
+  </LandingContainer>
+</template>
