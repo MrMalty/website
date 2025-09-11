e4283bcd476beb8ec2d78e6cc2c4e234adee8793
ellascreations <maltys@live.com.au>
2025-09-11 18:57:50 +1000

    .

--- components/landing/Products.vue
+++ components/landing/Products.vue
@@ -4,27 +4,23 @@ defineProps(["plan"]);
 
 <template>
   <div>
-    <div
-      class="flex flex-col h-[550px] w-[310px] order-first lg:order-none border-2 border-[#D8DEE9] border-opacity-50 py-5 px-6 rounded-md"
-    >
-      <div class="group relative">
-        <img
-          src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg"
-          alt="Front of men&#039;s Basic Tee in black."
-          class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-        />
-        <div class="mt-4 flex justify-between">
-          <div>
-            <h3 class="text-sm text-gray-700">
-              <a href="#">
-                <span aria-hidden="true" class="absolute inset-0"></span>
-                Basic Tee
-              </a>
-            </h3>
-            <p class="mt-1 text-sm text-gray-500">Black</p>
-          </div>
-          <p class="text-sm font-medium text-gray-900">$35</p>
+    <div class="group relative">
+      <img
+        src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg"
+        alt="Front of men&#039;s Basic Tee in black."
+        class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+      />
+      <div class="mt-4 flex justify-between">
+        <div>
+          <h3 class="text-sm text-gray-700">
+            <a href="#">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+              Basic Tee
+            </a>
+          </h3>
+          <p class="mt-1 text-sm text-gray-500">Black</p>
         </div>
+        <p class="text-sm font-medium text-gray-900">$35</p>
       </div>
     </div>
   </div>
