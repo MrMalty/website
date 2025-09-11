62bfe9735bb93c8a062fc0c6cc59de26221b6070
ellascreations <maltys@live.com.au>
2025-09-11 19:02:18 +1000

    .

--- components/landing/Products.vue
+++ components/landing/Products.vue
@@ -8,7 +8,7 @@ defineProps(["plan"]);
       <img
         src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg"
         alt="Front of men&#039;s Basic Tee in black."
-        class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-60"
+        class="aspect-square rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-60"
       />
       <div class="mt-4 flex justify-between">
         <div>
