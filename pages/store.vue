e42ae8b847237eb366911ec76d63b3e4d7b4026c
ellascreations <maltys@live.com.au>
2025-10-19 12:31:36 +1100

    update store

--- pages/store.vue
+++ pages/store.vue
@@ -5,6 +5,8 @@ definePageMeta({
 import { useHead } from "#app";
 import { onMounted } from "vue";
 
+const isOpen = ref(false);
+
 useHead({
   title: "Kialla Computers - Services",
 });
@@ -12,20 +14,14 @@ useHead({
 const supabase = useSupabaseClient();
 const categories = ref([]);
 const frontpageitems = ref([]);
-const primarycategories = ref([]);
 
 onMounted(async () => {
   const { data, error } = await supabase
     .from("categories")
     .select()
     .order("categoryname", { ascending: true });
-
-  const primarycategories = data.filter(
-    (item) => item.parentcategoryid === null
-  );
-
   categories.value = data;
-  console.log(data);
+  console.log(data, error);
 });
 
 onMounted(async () => {
@@ -48,8 +44,25 @@ onMounted(async () => {
     <div class="mt-6 flex flex-cols mx-auto">
       <div class="mt-6 w-[300px] px-6">
         <p class="border-b border-FooterText mb-5">Categories:</p>
-        <div v-for="cat in categories" :key="categories.id" class="">
-          {{ cat.categoryname }}
+        <div v-for="(cat, id) in categories" :key="id" class="">
+          <ul>
+            <li class="px-4 py-2 font-semibold">
+              <a v-if="cat.categoryitems === null">{{ cat.categoryname }}</a>
+              <a v-else href="/store">{{ cat.categoryname }}</a>
+            </li>
+            <ul
+              v-if="cat.categoryitems"
+              class="transition-all duration-300 ease-in-out px-6 py-2 list-none"
+            >
+              <li
+                v-for="item in cat.categoryitems"
+                :key="item"
+                class="py-1 font-light"
+              >
+                <NuxtLink to="/store">{{ item }}</NuxtLink>
+              </li>
+            </ul>
+          </ul>
         </div>
       </div>
 
